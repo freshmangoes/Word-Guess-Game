4dcf9b7150417a6582a296deff9999b78f7cd2b4
Kyle C <kylecilia@gmail.com>
2019-10-08 19:45:57 -0700

    Adds some more methods, cleans up code. Creates an array for theWord now.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,9 +6,10 @@ var gameState = {
     'alternator', 'axle', 'exhaust', 'intake', 'coilpack', 'gauges', 'harness', 'fender', 'trunk', 'hood', 'door', 'solenoid', 'relay', 'fuse', 'wheel', 'tire', 'bearing', 'transmission', 'differential', 'frame', 'mount', 'motor', 'hub', 'knuckle', 'rack', 'pinion', 'piston', 'rod', 'cam', 'sensor', 'bumper', 'brakes', 'rotor', 'caliper', 'headlight', 'airbox',
   ],
   guessedLetters: [],
-  wins: 0,
+  theWordArray: [],
   theWord: "",
-  guesses: 15,
+  guesses: 7,
+  wins: 0,
   pickWord: function () {
     // Gets list length
     var listLength = this.carParts.length - 1;
@@ -19,9 +20,39 @@ var gameState = {
     // Sets theWord to the result
     this.theWord = result;
   },
+
+  // Adds more guesses based off of the length of the goal word
+  addGuesses(){
+    this.guesses += this.theWord.length;
+    console.log('Remaining guesses:', this.guesses);
+  },
+
+  // populates theWordArray 
+  setWordArray(word) {
+    // splits a word into individual characters
+    // pushes them to theWordArray
+    word.split('').forEach(c => {
+      this.theWordArray.push(c);
+    });
+  },
+
+  // Sets the gameState
+  init() {
+    // picks the word
+    this.pickWord();
+    // sets the number of guesses
+    this.addGuesses();
+    // creates the array for theWord
+    this.setWordArray(this.theWord);
+  }
+
 }
 
 // Debug statement, making sure gameState is picking a word
-gameState.pickWord();
+
+// gameState.pickWord();
+// gameState.addGuesses();
+// gameState.setWordArray(gameState.theWord);
+gameState.init();
+console.log(gameState.theWordArray);
 console.log('Game word:', gameState.theWord);
-console.log(typeof (gameState.theWord));
