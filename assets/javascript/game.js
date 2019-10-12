15218c2b69ae97cc07c1c559d6f328a147429eac
Kyle C <kylecilia@gmail.com>
2019-10-12 00:12:12 -0700

    Adds function to fill in correct guesses. Adds function to get indicies of correct guess. Changes some function names to improve readability.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,18 @@
-
+// aux
+function getIndex(array, value) {
+  // array to store index values
+  var indicies = [];
+  // loop through the array to be searched for indicies
+  for (var i = 0; i < array.length; i++){
+    // check indicies against value
+    if (array[i] === value) {
+      // pushes correct index to indicies[]
+      indicies.push(i);
+    }
+  }
+  // returns correct indicies
+  return indicies;
+}
 
 
 var gameState = {
@@ -29,13 +43,13 @@ var gameState = {
   },
 
   // Adds more guesses based off of the length of the goal word
-  addGuesses(){
+  initGuesses() {
     this.guesses += this.theWord.length;
     console.log('Remaining guesses:', this.guesses);
   },
 
   // populates theWordArray 
-  setWordArray(word) {
+  initWordAray(word) {
     // splits a word into individual characters
     // pushes them to theWordArray
     word.split('').forEach(c => {
@@ -44,9 +58,53 @@ var gameState = {
   },
 
   // creates an array of underscores
-  setFillInArray(arrayLength){
+  initFillArray(arrayLength) {
     for (let i = 0; i < arrayLength; i++) {
       this.fillInArray.push('_');
+    }
+  },
+
+  setFillArray(letter, answer, fill) {
+    // gets array of indicies that contain the letter
+    var correctIndicies = getIndex(answer, letter);
+    // if correctIndicies has more than one index
+    if(correctIndicies.length != 1) {
+      // iterate
+      for(var i = 0; i < correctIndicies.length - 1; i++){
+        // get the index by taking the value of the current iteration
+        var index = correctIndicies[i];
+        // set fill array at that index to the answer array at that index
+        fill[index] = answer[index];
+      } 
+    } else {
+      // case for only having one index
+      var index = correctIndicies[0];
+      fill[index] = answer[index];
+    }
+  },
+
+  addGuess(letter) {
+    // case for already guessed letter
+    if(this.fillInArray.includes(letter)) {
+      console.log('Already guessed', letter);
+    // case for correct guess
+    } else if (this.theWordArray.includes(letter)) {
+      console.log('Correct guess');
+      // add letter to guessedLetters array
+      this.guessedLetters.push(letter);
+      // add new letter to fillInArray based on index in theWordArray
+      this.setFillArray(letter, this.theWordArray, this.fillInArray);
+      // subtract one from guesses
+      this.guesses = this.guesses - 1;
+      
+    // case for incorrect guess
+    } else {
+      console.log('Chosen word does not contain this character:', letter);
+      // push letter to guessed array
+      this.guessedLetters.push(letter);
+      console.log('Guessed letters: ', this.guessedLetters);
+      // subtract one from guesses
+      this.guesses = this.guesses - 1;
     }
   },
 
@@ -55,11 +113,11 @@ var gameState = {
     // picks the word
     this.pickWord();
     // sets the number of guesses
-    this.addGuesses();
+    this.initGuesses();
     // creates the array for theWord
-    this.setWordArray(this.theWord);
+    this.initWordAray(this.theWord);
     // creates the array for correctly guessed words
-    this.setFillInArray(this.theWordArray.length);
+    this.initFillArray(this.theWordArray.length);
     // console.log(this.fillInArray);
   }
 
