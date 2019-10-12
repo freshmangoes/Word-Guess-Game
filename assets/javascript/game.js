61dfcbef18d9f154125cc705da1aff63871eba03
Kyle C <kylecilia@gmail.com>
2019-10-12 13:16:03 -0700

    Adds an update method to gameState to clean up code, now displaying remaining guesses correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ function getIndex(array, value) {
   // array to store index values
   var indicies = [];
   // loop through the array to be searched for indicies
-  for (var i = 0; i < array.length; i++){
+  for (var i = 0; i < array.length; i++) {
     // check indicies against value
     if (array[i] === value) {
       // pushes correct index to indicies[]
@@ -67,15 +67,15 @@ var gameState = {
     // gets array of indicies that contain the letter
     var correctIndicies = getIndex(answer, letter);
     // if correctIndicies has more than one index
-    if(correctIndicies.length != 1) {
+    if (correctIndicies.length != 1) {
       // iterate
-      for(var i = 0; i < correctIndicies.length; i++){
+      for (var i = 0; i < correctIndicies.length; i++) {
         console.log('setFillArray - correctIndicies: ', correctIndicies);
         // get the index by taking the value of the current iteration
         var index = correctIndicies[i];
         // set fill array at that index to the answer array at that index
         fill[index] = answer[index];
-      } 
+      }
     } else {
       // case for only having one index
       var index = correctIndicies[0];
@@ -85,9 +85,9 @@ var gameState = {
 
   addGuess(letter) {
     // case for already guessed letter
-    if(this.fillInArray.includes(letter)) {
+    if (this.fillInArray.includes(letter)) {
       console.log('Already guessed', letter);
-    // case for correct guess
+      // case for correct guess
     } else if (this.theWordArray.includes(letter)) {
       console.log('Correct guess');
       // add letter to guessedLetters array
@@ -97,8 +97,8 @@ var gameState = {
       // subtract one from guesses
       this.guesses = this.guesses - 1;
       console.log('Remaining guesses: ', this.guesses);
-      
-    // case for incorrect guess
+
+      // case for incorrect guess
     } else {
       console.log('Chosen word does not contain this character:', letter);
       // push letter to guessed array
@@ -112,7 +112,7 @@ var gameState = {
     console.log('gameState.guessedLetters: ', gameState.guessedLetters);
   },
 
-  checkWinCondition() { 
+  checkWinCondition() {
     // makes fillInArray a string
     var fillInWord = this.fillInArray.join('');
     // compares chosen word against fillInArray word
@@ -137,47 +137,53 @@ var gameState = {
     // creates the array for correctly guessed words
     this.initFillArray(this.theWordArray.length);
     console.log('Fill in array: ', this.fillInArray);
-  }
+  },
 
+  update() {
+    // get elements to be modified and set them to variables
+    var targetWord = document.getElementById('target-word');
+    var fillArrayDisplay = document.getElementById('fill-display');
+    var winCount = document.getElementById('win-count');
+    var guessCount = document.getElementById('guess-count');
+    var guessArrayDisplay = document.getElementById('guess-display');
+
+    // populating HTML with gameState data
+    targetWord.innerHTML = this.theWord;
+    fillArrayDisplay.innerHTML = this.fillInArray.join(' ');
+    guessArrayDisplay.innerHTML = this.guessedLetters.join(', ');
+    winCount.innerHTML = 'Win Count: ' + this.wins;
+    guessCount.innerHTML = 'Guesses Left: ' + this.guesses;
+
+    // onkeyup event to capture key input
+    document.onkeyup = function (event) {
+
+      var guessCode = event.keyCode;
+      // if statement to make sure a letter was pressed
+      // if a letter is pressed, update gameState and HTML
+      if (guessCode >= 65 && guessCode <= 90) {
+        console.log('input was a letter');
+        var guess = String.fromCharCode(guessCode).toLowerCase();
+        console.log('Guess: ', guess);
+        gameState.addGuess(guess);
+
+        // updates HTML after pressing a key, for whatever reason will not
+        // work with the this keyword
+        fillArrayDisplay.innerHTML = gameState.fillInArray.join(' ');
+        guessArrayDisplay.innerHTML = gameState.guessedLetters.join(', ');
+        guessCount.innerHTML = 'Remaining guesses: ' + gameState.guesses;
+
+        // do not update gameState and HTML if letter was not pressed
+      } else {
+        console.log('input was not a letter');
+      }
+    }
+  }
 }
 
 // Initialize gameState
 gameState.init();
 
-// get elements to be modified and set them to variables
-var targetWord = document.getElementById('target-word');
-var fillArrayDisplay = document.getElementById('fill-display');
-var winCount = document.getElementById('win-count');
-var guessArrayDisplay = document.getElementById('guess-display');
-
-// populating HTML with gameState data
-targetWord.innerHTML = gameState.theWord;
-
-fillArrayDisplay.innerHTML = gameState.fillInArray.join(' ');
-
-guessArrayDisplay.innerHTML = gameState.guessedLetters.join(', ');
-
-winCount.innerHTML = 'Win Count: ' + gameState.wins;
-
-// onkeyup event to capture key input
-document.onkeyup = function(event) {
-
-  var guessCode = event.keyCode;
-  // if statement to make sure a letter was pressed
-  // if a letter is pressed, update gameState and HTML
-  if(guessCode >= 65 && guessCode <= 90) {
-    console.log('input was a letter');
-    var guess = String.fromCharCode(guessCode).toLowerCase();
-    console.log('Guess: ', guess);
-    gameState.addGuess(guess);
-  
-    // updates HTML after pressing a key
-    fillArrayDisplay.innerHTML = gameState.fillInArray.join(' ');
-    guessArrayDisplay.innerHTML = gameState.guessedLetters.join(', ');
-  // do not update gameState and HTML if letter was not pressed
-  } else {
-    console.log('input was not a letter');
-  }
-}
+// updates gameState
+gameState.update();
 
 // Debug statement, making sure gameState is picking a word
